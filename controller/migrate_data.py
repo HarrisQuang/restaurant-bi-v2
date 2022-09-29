b584e5f7b5623d2463aaba28ee3630332d7af2e4
kimhai <kimhaiareal@gmail.com>
2022-09-29 22:14:26 +0700

    commit

--- controller/migrate_data.py
+++ controller/migrate_data.py
@@ -16,6 +16,7 @@ from datetime import date
 shutil.rmtree('temp')
 os.mkdir('temp', 0o755)
 
+print('Start getting files from data source')
 engine = create_engine("postgresql://postgres:12345678@localhost:5432/demo_db")
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
@@ -34,7 +35,9 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name(SERVICE_ACCOUNT_F
 access_token = credentials.create_delegated(credentials._service_account_email).get_access_token().access_token
 
 file_name_list = []
-for el in dict(res)['fileList'][0]['files']:
+file_list_from_source = dict(res)['fileList'][0]['files']
+for count, el in enumerate(file_list_from_source):
+    print(f"[{count+1}/{len(file_list_from_source)}] Processing the file {el['name']}")
     url = "https://www.googleapis.com/drive/v3/files/" + el['id'] + "/export?mimeType=application%2Fvnd.openxmlformats-officedocument.spreadsheetml.sheet"
     res = requests.get(url, headers={"Authorization": "Bearer " + access_token})
     with open(f"temp/{el['name']}.xlsx", 'wb') as f:
@@ -45,7 +48,7 @@ print(f'File list from data source: {file_name_list}')
 
 term_in_db = get_finance_data_term_list()
 
-print(f'Term list from DB: {term_in_db}')
+print(f'Existing term list from DB: {term_in_db}')
 
 file_name_migrate = []
 for i in file_name_list:
@@ -56,11 +59,15 @@ for i in file_name_list:
     if count == 0:
         file_name_migrate.append(i)
 
-print(f'{file_name_migrate}')
+if len(file_name_migrate) != 0:
+    print(f'Files need to be migrated: {file_name_migrate}')
+else:
+    print('No new files need to be migrated')
 
 # Transfrom data in staging
 # -> Export 2 DFs (1 df for finance, 1 df for order) with columns as per requirement
-for name in file_name_migrate:
+for count, name in enumerate(file_name_migrate):
+    print(f'[{count+1}/{len(file_name_migrate)}] Processing file: {name}')
     df = export_one_df_finance(name)
 
     # Load data into Postgres DB
@@ -72,7 +79,6 @@ for name in file_name_migrate:
             root = root + loop
         else:
             root = root + ", " + loop
-    print(root)
 
     ist_val = []
     for id, row in df.iterrows():
@@ -88,19 +94,16 @@ for name in file_name_migrate:
     query_stmnt = "INSERT INTO finance (NGAY_NUMBER, KY, NGAY, DOANH_THU, CHI_PHI, NET_SP_FOOD, GRAB, BAEMIN, CK_SP_FOOD, SP_FOOD, CK_GRAB, CK_BAEMIN, TAI_QUAN, PCT_BAEMIN, PCT_GRAB, PCT_SP_FOOD, PCT_TAI_QUAN) " + root % ist_val
     engine.execute(query_stmnt)
 
+
 today = str(date.today())
 current_term = 'THU CHI T' + today[6:7] + '-' + today[2:4]
 current_term = 'THU CHI T9-22'
 df = export_one_df_finance(current_term)
 if df != None:
     ngay_number_from_source_max = int(df[df['DOANH-THU'] != 0]['NGAY-NUMBER'].max())
-    print(ngay_number_from_source_max)
-    print(type(ngay_number_from_source_max))
 
     df1 = get_current_term_finance_db()
     ngay_number_from_db_max = df1[df1['doanh_thu'] != 0]['ngay_number'].max()
-    print(ngay_number_from_db_max)
-    print(type(ngay_number_from_db_max))
 
     ngay_number_list_to_add = []
     if ngay_number_from_source_max > ngay_number_from_db_max:
@@ -108,22 +111,25 @@ if df != None:
         for i in range(count):
             ngay_number_from_db_max = ngay_number_from_db_max + 1
             ngay_number_list_to_add.append(ngay_number_from_db_max)
-        print(df)
-        print(ngay_number_list_to_add)
-
+        
+        print(f'List of days need to be appended: {ngay_number_list_to_add}')
+        
+        part_stmt = "DOANH_THU = '%s', CHI_PHI = '%s', NET_SP_FOOD = '%s', GRAB = '%s', BAEMIN = '%s', CK_SP_FOOD = '%s', SP_FOOD = '%s', CK_GRAB = '%s', CK_BAEMIN = '%s', TAI_QUAN = '%s', PCT_BAEMIN = '%s', PCT_GRAB = '%s', PCT_SP_FOOD = '%s', PCT_TAI_QUAN = '%s' "
         new_dict = {}
-        for i in ngay_number_list_to_add:
+        for count, i in enumerate(ngay_number_list_to_add):
+            print(f'[{count + 1}/{len(ngay_number_list_to_add)}] The ngay_number {i} are adding')
             record = list(df[df['NGAY-NUMBER'] == str(i)][['DOANH-THU', 'CHI-PHI', 'NET-SP-FOOD', 'GRAB', 'BAEMIN', 'CK-SP-FOOD', 'SP-FOOD', 'CK-GRAB', 'CK-BAEMIN', 'TAI-QUAN', 'PCT-BAEMIN', 'PCT-GRAB', 'PCT-SP-FOOD', 'PCT-TAI-QUAN']].values[0])
             record.append(i)
             record = tuple(record)
-            new_dict[i] = record
-        print(new_dict)
-
-        part_stmt = "DOANH_THU = '%s', CHI_PHI = '%s', NET_SP_FOOD = '%s', GRAB = '%s', BAEMIN = '%s', CK_SP_FOOD = '%s', SP_FOOD = '%s', CK_GRAB = '%s', CK_BAEMIN = '%s', TAI_QUAN = '%s', PCT_BAEMIN = '%s', PCT_GRAB = '%s', PCT_SP_FOOD = '%s', PCT_TAI_QUAN = '%s' "
-
-        for i in new_dict.values():
-            print(i)
+            # new_dict[i] = record
             final_stmt = "UPDATE finance SET " + part_stmt + "WHERE ngay_number = '%s'"
-            final_stmt = final_stmt % i
-            print(final_stmt)
+            final_stmt = final_stmt % record
             engine.execute(final_stmt)
+        # for i in new_dict.values():
+        #     final_stmt = "UPDATE finance SET " + part_stmt + "WHERE ngay_number = '%s'"
+        #     final_stmt = final_stmt % i
+        #     engine.execute(final_stmt)
+    else:
+        print("There's no any new records needed to be fetched")
+else:
+    print(f"The current term {current_term} is not available")
